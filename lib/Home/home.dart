0a1f6f94f1c88b8dbdc913bd00edfaa688172bd4
Felipe Abreu <felipeabreu.rj@gmail.com>
2021-08-11 19:08:57 -0300

    Se aplicado, este commit irá adicionar a lógica do jogo

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,14 +9,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var opcoesJogo = ["papel", "pedra", "tesoura"];
-  var escolhaUsuario = -1;
-  var escolhaApp = -1;
+  var imagemApp = AssetImage("assets/images/padrao.png");
+  var mensagem = "Escolha uma opção abaixo:";
+  int vitorias = 0;
+  int derrotas = 0;
+  int empates = 0;
 
-  void jogar() {
-    escolhaUsuario = Random().nextInt(opcoesJogo.length);
-    escolhaApp = Random().nextInt(opcoesJogo.length);
-    setState(() {});
+  void validarJogada(String escolhaUsuario) {
+    var opcoesJogo = ["papel", "pedra", "tesoura"];
+    var escolhaApp = opcoesJogo[Random().nextInt(opcoesJogo.length)];
+
+    // Atualização da escolha do app
+    switch (escolhaApp) {
+      case "pedra":
+        setState(() {
+          this.imagemApp = AssetImage("assets/images/pedra.png");
+        });
+        break;
+      case "papel":
+        setState(() {
+          this.imagemApp = AssetImage("assets/images/papel.png");
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          this.imagemApp = AssetImage("assets/images/tesoura.png");
+        });
+        break;
+    }
+
+    // Validação do vencedor
+    if ((escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+        (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+        (escolhaUsuario == "papel" && escolhaApp == "pedra")) {
+      setState(() {
+        this.vitorias++;
+        this.mensagem = "Parabéns!!! Você ganhou :)";
+      });
+    } else if ((escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+        (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+        (escolhaApp == "papel" && escolhaUsuario == "pedra")) {
+      setState(() {
+        this.derrotas++;
+        this.mensagem = "Você perdeu :(";
+      });
+    } else {
+      setState(() {
+        this.empates++;
+        this.mensagem = "Empatamos ;)";
+      });
+    }
   }
 
   @override
@@ -32,43 +73,90 @@ class _HomeState extends State<Home> {
       ),
       body: Center(
         child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Padding(
-              padding: EdgeInsets.only(top: 32, bottom: 16),
+              padding: EdgeInsets.only(top: 0, bottom: 16),
               child: Text(
                 "Escolha do App:",
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
-            Image.asset("assets/images/padrao.png"),
+            Image(image: this.imagemApp),
             Padding(
-              padding: EdgeInsets.only(top: 32, bottom: 16),
-              child: Text(
-                "Escolha uma opção abaixo:",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
+                padding: EdgeInsets.only(top: 32, bottom: 16),
+                child: Text(this.mensagem,
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Image.asset(
-                  "assets/images/pedra.png",
-                  height: 95,
+                GestureDetector(
+                  onTap: () => validarJogada("pedra"),
+                  child: Image.asset(
+                    "assets/images/pedra.png",
+                    height: 100,
+                  ),
                 ),
-                Image.asset(
-                  "assets/images/papel.png",
-                  height: 95,
+                GestureDetector(
+                  onTap: () => validarJogada("papel"),
+                  child: Image.asset(
+                    "assets/images/papel.png",
+                    height: 100,
+                  ),
                 ),
-                Image.asset(
-                  "assets/images/tesoura.png",
-                  height: 95,
+                GestureDetector(
+                  onTap: () => validarJogada("tesoura"),
+                  child: Image.asset(
+                    "assets/images/tesoura.png",
+                    height: 100,
+                  ),
                 ),
               ],
             ),
+            Padding(
+              padding: EdgeInsets.only(top: 60),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 5, bottom: 5),
+                    padding: EdgeInsets.only(top: 5, bottom: 5),
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 3, color: Colors.black26),
+                        color: Colors.black12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Você: $vitorias",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue),
+                        ),
+                        Text(
+                          "App: $derrotas",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red),
+                        ),
+                        Text(
+                          "Empates: $empates",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
